3d875ae614261234cb04a1d67ace5ed2d1f3a147
Eugeniy <yarastqt@gmail.com>
2017-08-24 21:32:10 +0300

    Добавил компонент - Icon (атом) #WF-16
    Добавил иконки: exit, star, geolocation, search. Исправил конфиг вебпака для сторибук

---
+++ src/ui/atoms/icon/icon.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import styled, { css } from 'styled-components'
+
+import * as icons from 'icons'
+
+
+const IconWrapper = styled.i`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: ${p => p.size}px;
+  height: ${p => p.size}px;
+
+  svg {
+    transition: fill 150ms ease-in-out;
+    max-width: ${p => p.size}px;
+    max-height: ${p => p.size}px;
+    ${p => p.fill && css`
+      fill: ${p.fill};
+    `}
+    ${p => p.stroke && css`
+      stroke: ${p.stroke};
+    `}
+  }
+`
+
+const Icon = ({ name, ...props }) => {
+  const Svg = icons[name] || null
+
+  return (
+    <IconWrapper {...props}>
+      <Svg />
+    </IconWrapper>
+  )
+}
+
+Icon.propTypes = {
+  name: PropTypes.oneOf(Object.keys(icons)).isRequired,
+  fill: PropTypes.string,
+  stroke: PropTypes.string,
+  size: PropTypes.number,
+}
+
+Icon.defaultProps = {
+  fill: '',
+  stroke: '',
+  size: 24,
+}
+
+export default Icon
