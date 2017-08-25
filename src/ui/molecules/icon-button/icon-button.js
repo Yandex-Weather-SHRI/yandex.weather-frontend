791a69c3b8f3437e25573567f0b30275d4c7c383
Михаил Кононенко <vbifonixor@gmail.com>
2017-08-25 19:11:55 +0300

    Создал молекулу кнопки с иконкой #WF-15

---
+++ src/ui/molecules/icon-button/icon-button.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+
+import { Icon } from '../atoms'
+
+
+const IconButtonWrapper = styled.a`
+  &,
+  &:link,
+  &:visited,
+  &:hover,
+  &:active {
+    text-decoration: none;
+    color: initial;
+  }
+`
+
+const IconButton = ({ iconName, onClick }) => (
+  <IconButtonWrapper onClick={onClick}>
+    <Icon name={iconName} />
+  </IconButtonWrapper>
+)
+
+IconButton.propTypes = {
+  iconName: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired,
+}
+
+export default IconButton
