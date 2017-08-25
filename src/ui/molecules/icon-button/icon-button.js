ef1a5e4ec8574ab5c7b75e586cc9a2e39ff9f7df
Михаил Кононенко <vbifonixor@gmail.com>
2017-08-25 20:04:41 +0300

    Изменил путь к компоненту Icon на его алиас #WF-15

--- src/ui/molecules/icon-button/icon-button.js
+++ src/ui/molecules/icon-button/icon-button.js
@@ -2,7 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
 
-import { Icon } from '../../atoms'
+import { Icon } from 'ui/atoms'
 
 
 const IconButtonWrapper = styled.button`
