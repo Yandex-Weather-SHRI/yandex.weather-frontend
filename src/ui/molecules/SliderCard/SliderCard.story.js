1d3457159f1c0ffca29b06ed63b64b7f13c40bde
Михаил Кононенко <vbifonixor@gmail.com>
2017-08-26 21:05:47 +0300

    Добавил пару примеров использования в сторибук (без подробностей) #WF-17

---
+++ src/ui/molecules/SliderCard/SliderCard.story.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import { withInfo } from '@storybook/addon-info'
+import { action } from '@storybook/addon-actions'
+
+import { SliderCard } from './SliderCard'
+
+
+storiesOf('molecules/SliderCard', module)
+  .add('SliderCard',
+    withInfo()(() => (
+      <div style={{ fontFamily: 'sans-serif' }}>
+        <div style={{ margin: '40px 0' }}>
+          <SliderCard buttonText="Товарища нет" iconName="location_arrow">
+            На <strong>вкус</strong> и цвет
+          </SliderCard>
+        </div>  
+        <SliderCard buttonText="Купить сало" bg="linear-gradient(to bottom, #9cddff, #4baffd 52%, #498ffa);" color="#fff" borderColor="#fff">
+          <strong>Наше сало самое недорогое, а ещё неплохо согревает!</strong>
+        </SliderCard>
+      </div>
+    ))
+  )
