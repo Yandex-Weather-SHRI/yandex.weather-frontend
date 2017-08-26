c4a39cfcf8126ba4840079e507dfca2a911d5d37
Artem Maksimov <temamaksimov@gmail.com>
2017-08-26 13:32:30 +0300

    Рефакторинг: удалил index.js, переименовал компоненту в CamelCase #WF-18

--- src/ui/atoms/Title/Title.story.js
+++ src/ui/atoms/Title/Title.story.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { storiesOf } from '@storybook/react'
 import { withInfo } from '@storybook/addon-info'
 
-import Title from './title'
+import Title from './Title'
 
 
 storiesOf('atoms', module)
