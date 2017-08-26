d9633c65dde55dc0536b825af57df0eef436ec84
Михаил Кононенко <vbifonixor@gmail.com>
2017-08-26 13:44:45 +0300

    Впилил пропсы fill и stroke в Icon (и обновил файл сторибука) #WF-15

---
+++ src/ui/molecules/IconButton/IconButton.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+
+import { Icon } from 'ui/atoms'
+
+
+const IconButtonWrapper = styled.button`
+  border: none;
+  background: transparent;
+  cursor: pointer;
+  min-width: 32px;
+  height: 32px;
+  padding: 0 4px;
+  margin: 0;
+  font: inherit;
+
+  &:focus {
+    outline: 0;
+  }
+`
+const Wrapper = styled.span`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+`
+
+const Text = styled.span`
+  font-size: 1.4rem;
+  font-family: sans-serif;
+  padding: 5px;
+`
+
+const IconButton = ({ iconName, onClick, children, fill, stroke, ...props }) => (
+  <IconButtonWrapper onClick={onClick} {...props}>
+    <Wrapper>
+      <Icon name={iconName} fill={fill} stroke={stroke} />
+      {children && <Text>{children}</Text>}
+    </Wrapper>
+  </IconButtonWrapper>
+)
+
+IconButton.propTypes = {
+  iconName: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired,
+  children: PropTypes.node.isRequired,
+  fill: PropTypes.string,
+  stroke: PropTypes.string,
+}
+
+IconButton.defaultProps = {
+  fill: null,
+  stroke: null,
+}
+
+export default IconButton
---
+++ src/ui/molecules/IconButton/IconButton.story.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import { withInfo } from '@storybook/addon-info'
+import { action } from '@storybook/addon-actions'
+
+import IconButton from './IconButton'
+
+
+storiesOf('molecules/icon-button', module)
+  .add('icon-buttons',
+    withInfo()(() => (
+      <div>
+        <div>
+          <IconButton iconName="star" onClick={action('star button clicked!')} />
+          <IconButton iconName="exit" onClick={action('exit button clicked!')} />
+          <IconButton iconName="geolocation" onClick={action('geolocation button clicked!')} />
+          <IconButton iconName="search" onClick={action('search button clicked!')} />
+        </div>
+        <div style={{ backgroundColor: '#000' }}>
+          <IconButton fill="#fff" iconName="star" onClick={action('white star button clicked')} />
+        </div>
+      </div>
+    ))
+  )
+  .add('icon-button-with-text',
+    withInfo()(() => (
+      <div>
+        <IconButton iconName="search" onClick={action('Search button with text clicked!')}>Поиск</IconButton>
+      </div>
+    ))
+  )
