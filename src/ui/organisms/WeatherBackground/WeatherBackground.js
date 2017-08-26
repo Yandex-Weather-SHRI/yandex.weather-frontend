a3d1ed0d0f96bcace5c06be2d011aeaa27a06c49
Eugeniy <yarastqt@gmail.com>
2017-08-26 18:27:43 +0300

    Обновил компонент - WeatherBackground

--- src/ui/organisms/WeatherBackground/WeatherBackground.js
+++ src/ui/organisms/WeatherBackground/WeatherBackground.js
@@ -2,21 +2,10 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
 
-import good1 from 'assets/images/weather-backgrounds/good-1.png'
-import good2 from 'assets/images/weather-backgrounds/good-2.png'
-import good3 from 'assets/images/weather-backgrounds/good-3.png'
 import { NavigationBar } from 'ui/molecules'
 
+import { BACKGROUNDS, getWeatherBackground } from './backgrounds'
 
-const BACKGROUNDS = {
-  good: [good1, good2, good3],
-}
-
-const randomBackground = Math.round(Math.random() * (BACKGROUNDS.good.length - 1))
-
-function getWeatherBackground(props) {
-  return BACKGROUNDS[props.condition][randomBackground]
-}
 
 const Container = styled.div`
   min-height: 304px;
@@ -27,7 +16,7 @@ const Container = styled.div`
 `
 
 export const WeatherBackground = ({ condition }) => (
-  <Container condition={condition}>
+  <Container {...{ condition }}>
     <NavigationBar />
   </Container>
 )
