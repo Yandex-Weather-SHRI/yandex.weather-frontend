6381de7768ffd5e67e191ef2e9ce51072cc8b059
Михаил Кононенко <vbifonixor@gmail.com>
2017-08-26 11:51:29 +0300

    Добавил белую иконкокнопку в сторибук

--- src/ui/molecules/icon-button/icon-button.story.js
+++ src/ui/molecules/icon-button/icon-button.story.js
@@ -10,10 +10,15 @@ storiesOf('molecules/icon-button', module)
   .add('icon-buttons',
     withInfo()(() => (
       <div>
-        <IconButton iconName="star" onClick={action('star button clicked!')} />
-        <IconButton iconName="exit" onClick={action('exit button clicked!')} />
-        <IconButton iconName="geolocation" onClick={action('geolocation button clicked!')} />
-        <IconButton iconName="search" onClick={action('search button clicked!')} />
+        <div>
+          <IconButton iconName="star" onClick={action('star button clicked!')} />
+          <IconButton iconName="exit" onClick={action('exit button clicked!')} />
+          <IconButton iconName="geolocation" onClick={action('geolocation button clicked!')} />
+          <IconButton iconName="search" onClick={action('search button clicked!')} />
+        </div>
+        <div style={{ backgroundColor: '#000' }}>
+          <IconButton fill="#fff" iconName="star" onClick={action('white star button clicked')} />
+        </div>
       </div>
     ))
   )
