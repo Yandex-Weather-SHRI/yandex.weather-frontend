c9885fd2122d3747f23809cbddb836c78c2d67c0
Eugeniy <yarastqt@gmail.com>
2017-08-26 15:18:02 +0300

    Добавил глобальные стили, Добавил шрифт

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { AppContainer } from 'react-hot-loader'
 import { ThemeProvider } from 'styled-components'
 
 import configureStore from './store'
+import './styles/global'
 
 
 const theme = {}
---
+++ src/styles/fonts.js
@@ -0,0 +1,11 @@
+import { css } from 'styled-components'
+
+import HelveticaRegular from 'assets/fonts/HelveticaRegular.ttf'
+
+
+export const fonts = css`
+  @font-face {
+    font-family: Helvetica;
+    src: url('${HelveticaRegular}') format('truetype');
+  }
+`
