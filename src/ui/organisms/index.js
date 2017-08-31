e5f2a9d084fe436fa91b297bacaa9b32221b11ef
Artem Maksimov <temamaksimov@gmail.com>
2017-08-31 19:21:11 +0300

    Добавил компонент AppBar (организм) #WF-64

--- src/ui/organisms/index.js
+++ src/ui/organisms/index.js
@@ -1,3 +1,4 @@
 export { WeatherBlock } from './WeatherBlock/WeatherBlock'
 export { WeatherBackground } from './WeatherBackground/WeatherBackground'
 export { Slider } from './Slider/Slider'
+export { AppBar } from './AppBar/AppBar'
