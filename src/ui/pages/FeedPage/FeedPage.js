8d3b2506ada3ff8f28a55da27d55109b89601bf7
Alexander Gazizov <alexandr.bbm@gmail.com>
2017-08-31 20:48:48 +0300

    Добавил базовую верстку карточки #WF-33

--- src/ui/pages/FeedPage/FeedPage.js
+++ src/ui/pages/FeedPage/FeedPage.js
@@ -1,9 +1,33 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
+import styled from 'styled-components'
 
 import { getFeed } from 'redux/feed/actions'
+import { FeedCard } from 'ui/organisms/FeedCard/FeedCard'
 
+const MOCK_CARDS = [
+  {
+    categoryGroupName: 'Метеозависимость',
+    categoryName: 'Сердце',
+    text: 'Людям с заболеваниями сердца желательно уменьшить физическую активность',
+    onShareClick: () => console.log('share!'),
+    onOptionsClick: () => console.log('options!'),
+    id: 1,
+  },
+  {
+    categoryGroupName: 'Метеозависимость',
+    categoryName: 'Астма',
+    text: 'Для людей с заболеванием астмы рекомендуем выбрать спокойную деятельность',
+    onShareClick: () => console.log('share!'),
+    onOptionsClick: () => console.log('options!'),
+    id: 2,
+  },
+]
+
+const Container = styled.div`
+  padding: 16px 8px 0;
+`
 
 class FeedPageContainer extends Component {
   static propTypes = {
@@ -24,14 +48,14 @@ class FeedPageContainer extends Component {
     const { list } = this.props
 
     return (
-      <div>
-        FeedsPage
+      <Container>
         {list.map(item => (
           <div key={item.id}>
             {item.text}
           </div>
         ))}
-      </div>
+        {MOCK_CARDS.map(card => <FeedCard {...card} key={card.id} />)}
+      </Container>
     )
   }
 }
