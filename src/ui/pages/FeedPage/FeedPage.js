ac0df70b077152bc2a2bd54dc54ea9939b10c4c3
Alexander Gazizov <alexandr.bbm@gmail.com>
2017-09-01 19:57:31 +0300

    Интеграция хедера #WF-68

--- src/ui/pages/FeedPage/FeedPage.js
+++ src/ui/pages/FeedPage/FeedPage.js
@@ -10,8 +10,8 @@ import { getFeedByFilters } from 'redux/feed/selectors'
 import { setFeedFilter } from 'redux/filters/actions'
 import { categoryGroup } from 'constants/categoryGroup'
 import { categories, meteoaddictedCategory } from 'constants/categories'
-import { Link } from 'react-router-dom';
-import { routeNames } from 'utils/routeNames';
+import { Link } from 'react-router-dom'
+import { routeNames } from 'utils/routeNames'
 
 const MOCK_CARDS = [
   {
