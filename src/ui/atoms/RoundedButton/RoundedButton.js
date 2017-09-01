3ca615cfac2a095218c831d19a04c334e9eb5697
Eugeniy <yarastqt@gmail.com>
2017-09-01 18:39:05 +0300

    Вынес кнопку в общий компонент - RoundedButton

---
+++ src/ui/atoms/RoundedButton/RoundedButton.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+
+
+export const RoundedButton = styled.button`
+  display: flex;
+  align-items: center;
+  height: 36px;
+  border-radius: 18px;
+  padding: 0 16px;
+  font-size: 1rem;
+  font-weight: 500;
+  user-select: none;
+  text-transform: uppercase;
+  margin-right: 8px;
+  transition: opacity 150ms ease-in-out;
+  border: none;
+`
