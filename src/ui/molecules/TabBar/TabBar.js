2951997b4d373e58cc49e5d601256c3cd21531c6
Artem Maksimov <temamaksimov@gmail.com>
2017-09-01 21:07:19 +0300

    Добавил компонент TabBar (молекула) #WF-67

---
+++ src/ui/molecules/TabBar/TabBar.js
@@ -0,0 +1,78 @@
+import React from 'react'
+import styled, { css } from 'styled-components'
+import PropTypes from 'prop-types'
+
+import { RoundedButton } from 'ui/atoms'
+
+
+const Container = styled.div`
+  height: 56px;
+  background-color: rgba(0, 0, 0, 0.27);
+  overflow: hidden;
+`
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  height: 100%;
+  padding: 0 28px;
+  overflow-x: scroll;
+  -webkit-overflow-scrolling: touch;
+
+  &::-webkit-scrollbar {
+    display: none;
+  }
+
+  &:after {
+    content: '';
+    flex: 0 0 10px;
+  }
+`
+
+const Button = RoundedButton.extend`
+  height: 36px;
+  background: none;
+  border-radius: 100px;
+  border: solid 1px rgba(255, 255, 255, 0.5);
+  color: white;
+  font-size: 1.2rem;
+  font-weight: 500;
+  letter-spacing: 0.6px;
+  margin-right: 24px;
+  
+  ${p => !p.active && css`
+    opacity: 0.3;
+    border: none;
+  `}
+  
+  &:last-of-type{
+    margin-right: 0;
+  }
+`
+
+export const TabBar = ({ tabs, currentTab, onTabSelect }) => (
+  <Container>
+    <Wrapper>
+      {tabs.map(({ id, title }) => (
+        <Button
+          onClick={() => onTabSelect(id)}
+          active={id === currentTab}
+          key={id}
+        >
+          {title}
+        </Button>
+      ))}
+    </Wrapper>
+  </Container>
+)
+
+TabBar.propTypes = {
+  tabs: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      title: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+  currentTab: PropTypes.number.isRequired,
+  onTabSelect: PropTypes.func.isRequired,
+}
