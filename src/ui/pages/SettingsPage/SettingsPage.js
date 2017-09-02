50f7af274b6eaaa905f5f98e84c78904570d2242
Михаил Кононенко <vbifonixor@gmail.com>
2017-09-02 13:18:35 +0300

    Добавил AppBar на страницу настроек #WF-73

--- src/ui/pages/SettingsPage/SettingsPage.js
+++ src/ui/pages/SettingsPage/SettingsPage.js
@@ -1,6 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
+import { AppBar } from 'ui/organisms'
+import { IconButton } from 'ui/molecules'
+import { routeNames } from 'utils/routeNames'
 
 
 export const SettingsPage = () => (
-  <div>SettingsPage</div>
+  <div style={{ width: '100%' }}>
+    <AppBar
+      title="Настройки"
+      elementLeft={
+        <Link to={routeNames.feed}>
+          <IconButton icon="arrow-left" size="24" />
+        </Link>
+      }
+    />
+  </div>
 )
