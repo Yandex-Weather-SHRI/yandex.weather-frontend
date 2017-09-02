f75dd710617b1dc66055ca2cb3640bec8428650a
Eugeniy <yarastqt@gmail.com>
2017-09-02 17:15:24 +0300

    Добавил иконку

--- src/ui/organisms/FeedCard/metas/FeedCardMetaHeart.js
+++ src/ui/organisms/FeedCard/metas/FeedCardMetaHeart.js
@@ -1,12 +1,13 @@
 import React from 'react'
 
-import { FeedCardMetaText } from 'ui/atoms'
+import { FeedCardMetaIcon, FeedCardMetaText } from 'ui/atoms'
 import { FeedCardMeta, FeedCardMetaSection } from 'ui/molecules'
 
 
 export const FeedCardMetaHeart = () => (
   <FeedCardMeta>
     <FeedCardMetaSection>
+      <FeedCardMetaIcon name="weather-conditions/geomagnetic" size={24} />
       <FeedCardMetaText textSize={2.6}>6 / 8</FeedCardMetaText>
       <FeedCardMetaText textSize={2}>баллов</FeedCardMetaText>
     </FeedCardMetaSection>
