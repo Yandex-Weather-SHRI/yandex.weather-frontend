4011673db9b57f6e9a26c854ddd95376d4d80868
Eugeniy <yarastqt@gmail.com>
2017-09-04 12:17:17 +0300

    Исправил стили карточек настроек, Исправил размер иконок

--- src/ui/molecules/SettingsCard/SettingsCard.js
+++ src/ui/molecules/SettingsCard/SettingsCard.js
@@ -6,60 +6,70 @@ import { Icon } from 'ui/atoms'
 import { getCategoryGroupStyle } from 'styles/utils'
 
 
-const SettingsCardWrapper = styled.div`
+const Container = styled.div`
   ${getCategoryGroupStyle}
+  flex: 0 0 auto;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
-  padding: 12px;
+  padding: 8px 12px 10px;
   border-radius: 4px;
-  min-width: 144px;
+  width: 144px;
   height: 112px;
   margin-right: 8px;
   user-select: none;
+
+  &:last-of-type {
+    margin-right: 0;
+  }
 `
 
-const SettingsCardName = styled.h2`
+const Name = styled.div`
   font-size: 1.6rem;
   font-weight: 700;
-  margin-bottom: 8px;
+  line-height: 1.2;
 `
 
-const SettingsCardAdvices = styled.p`
+const Advices = styled.div`
+  margin-top: 8px;
   font-weight: 500;
   color: rgba(255, 255, 255, 0.7);
 `
 
-const SettingsCardContent = styled.div`
+const Content = styled.div`
   display: flex;
   flex-direction: column;
 `
 
-const SettingsCardTop = styled.div`
+const Heading = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
 `
 
-const Line = styled.div`
+const Line = styled.span`
   width: 56px;
   height: 2px;
   background-color: rgba(255, 255, 255, 0.3);
-  border-radius: 8px;
+  border-radius: 1px;
 `
 
+const statusIcons = {
+  true: 'check-circle',
+  false: 'add-circle',
+}
 
 export const SettingsCard = ({ group, catName, catAdvices, onClick, checked }) => (
-  <SettingsCardWrapper name={group} onClick={onClick}>
-    <SettingsCardTop>
+  <Container name={group} onClick={onClick}>
+    <Heading>
       <Line />
-      <Icon name={`settings_${checked}`} size={24} />
-    </SettingsCardTop>
-    <SettingsCardContent>
-      <SettingsCardName>{catName}</SettingsCardName>
-      <SettingsCardAdvices>{catAdvices}</SettingsCardAdvices>
-    </SettingsCardContent>
-  </SettingsCardWrapper>
+      <Icon name={statusIcons[checked]} size={24} />
+    </Heading>
+    <Content>
+      <Name>{catName}</Name>
+      <Advices>{catAdvices}</Advices>
+    </Content>
+  </Container>
 )
 
 SettingsCard.propTypes = {
