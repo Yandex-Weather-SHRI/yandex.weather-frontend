7f06f8cd240e4dcb435986422666d3595ee402e0
Михаил Кононенко <vbifonixor@gmail.com>
2017-09-05 20:02:25 +0300

    Добавил карточку для онбординга #WF-92

---
+++ src/ui/organisms/OnBoardingCard/OnBoardingCard.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import styled from 'styled-components'
+
+import { FeedCard } from 'ui/organisms'
+import { getCategoryGroupStyle } from 'styles/utils'
+
+
+const Container = styled.div`
+  ${p => getCategoryGroupStyle({ name: p.card.categoryGroup })}
+  width: 328px;
+  margin: 0 auto;
+  border-radius: 4px;
+`
+
+const Underlay = styled.div`
+  ${p => getCategoryGroupStyle({ name: p.categoryName })}
+  height: 245px;
+  position: relative;
+  z-index: -1;
+  bottom: 233px;
+  width: 296px;
+  border-radius: 4px;
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: -233px;
+  transform: translateY(233px);
+`
+
+export const OnBoardingCard = ({ card }) => (
+  <div>
+    <Container card={card}>
+      <FeedCard {...card} onShareClick={() => null} onOptionsClick={() => null} />
+    </Container>
+    <Underlay categoryName={card.categoryGroup} />
+  </div>  
+)
