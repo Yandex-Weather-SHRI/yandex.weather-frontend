77f2c5a21453946f8aa470231309cc2ff67da264
Eugeniy <yarastqt@gmail.com>
2017-09-05 18:50:22 +0300

    Добавил экшен для получения доступных фильтров

--- src/redux/filters/actions.js
+++ src/redux/filters/actions.js
@@ -2,3 +2,23 @@ import { createAction } from 'redux-act'
 
 
 export const setFeedFilter = createAction('feed.filter.set')
+export const getAvailableFiltersSuccess = createAction('available.filters.get.success')
+
+function findFilterInList(title, list) {
+  return list.find(item => item.title === title)
+}
+
+export function getAvailableFilters() {
+  return (dispatch, getState) => {
+    const { user: { settings: { schema, categories } } } = getState()
+    const filtersList = categories.reduce((acc, { enabled, group }) => {
+      const { title } = schema[group]
+      if (enabled && !findFilterInList(title, acc)) {
+        return [...acc, { title, name: group, active: false }]
+      }
+      return acc
+    }, [])
+
+    dispatch(getAvailableFiltersSuccess(filtersList))
+  }
+}
