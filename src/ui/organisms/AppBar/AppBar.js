28150f4bb7f72371d36a1dab791b62a30f1d3226
Eugeniy <yarastqt@gmail.com>
2017-09-07 14:01:26 +0300

    Добавил фиксированный AppBar

--- src/ui/organisms/AppBar/AppBar.js
+++ src/ui/organisms/AppBar/AppBar.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import styled from 'styled-components'
+import React, { PureComponent } from 'react'
+import styled, { css } from 'styled-components'
 import PropTypes from 'prop-types'
 
 
@@ -10,6 +10,16 @@ const Container = styled.div`
   align-items: center;
   justify-content: space-between;
   box-shadow: 0 1px 0 0 rgba(0, 0, 0, 0.08);
+  position: fixed;
+  z-index: 1000;
+  top: 0;
+  left: 0;
+  right: 0;
+  background-color: #fff;
+  transition: transform 150ms ease-in-out;
+  ${p => !p.fixed && css`
+    transform: translateY(-101%);
+  `}
 `
 
 const ElementWrapper = styled.div`
@@ -26,19 +36,53 @@ const Title = styled.span`
   font-weight: 500;
 `
 
-export const AppBar = ({ title, elementLeft, elementRight }) => (
-  <Container>
-    <ElementWrapper>
-      {elementLeft}
-    </ElementWrapper>
-    <Title>
-      {title}
-    </Title>
-    <ElementWrapper>
-      {elementRight}
-    </ElementWrapper>
-  </Container>
-)
+export class AppBar extends PureComponent {
+  constructor(props, context) {
+    super(props, context)
+    this.lastScrollPosition = 0
+    this.scrollDelta = 20
+    this.state = {
+      fixed: true,
+    }
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.toggleFixHeader)
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.toggleFixHeader)
+  }
+
+  toggleFixHeader = () => {
+    if (Math.abs(this.lastScrollPosition - window.pageYOffset) > this.scrollDelta) {
+      this.setState({
+        fixed: window.pageYOffset < this.lastScrollPosition,
+      })
+
+      this.lastScrollPosition = window.pageYOffset
+    }
+  }
+
+  render() {
+    const { fixed } = this.state
+    const { title, elementLeft, elementRight } = this.props
+
+    return (
+      <Container {...{ fixed }}>
+        <ElementWrapper>
+          {elementLeft}
+        </ElementWrapper>
+        <Title>
+          {title}
+        </Title>
+        <ElementWrapper>
+          {elementRight}
+        </ElementWrapper>
+      </Container>
+    )
+  }
+}
 
 AppBar.propTypes = {
   title: PropTypes.string.isRequired,
