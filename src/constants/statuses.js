fc06aed08a71766ae9d83a05fb278488f267ea53
Eugeniy <yarastqt@gmail.com>
2017-09-08 17:17:49 +0300

    Изменил стили компонента FeedCard

---
+++ src/constants/statuses.js
@@ -0,0 +1,11 @@
+export const statuses = {
+  best: 'best',
+  ok: 'ok',
+  bad: 'bad',
+}
+
+export const statusDisplayNames = {
+  [statuses.best]: 'Благоприятно',
+  [statuses.ok]: 'Нормально',
+  [statuses.bad]: 'Неблагоприятно',
+}
--- src/styles/utils.js
+++ src/styles/utils.js
@@ -1,6 +1,7 @@
 import { css } from 'styled-components'
 
 import { categoryGroup } from 'constants/categoryGroup'
+import { statuses } from 'constants/statuses'
 
 
 export function getCategoryGroupStyle({ name }) {
@@ -27,6 +28,34 @@ export function getCategoryGroupStyle({ name }) {
         background-color: #fff;
         box-shadow: 0 2px 10px 0 rgba(0, 0, 0, 0.07), 0 2px 17px 0 rgba(0, 0, 0, 0.01);
         border: solid 1px rgba(0, 0, 0, 0.08);
+      `
+  }
+}
+
+export function getStatusStyles(status) {
+  switch (status) {
+    case statuses.best:
+      return css`
+        color: #fff;
+        background-color: #2fd2b4;
+      `
+
+    case statuses.ok:
+      return css`
+        color: rgba(0, 0, 0, 0.7);
+        background-color: #fbdd4c;
+      `
+
+    case statuses.bad:
+      return css`
+        color: #fff;
+        background-color: #f05756;
+      `
+
+    default:
+      return css`
+        color: #fff;
+        background-color: #f6f7f8;
       `
   }
 }
