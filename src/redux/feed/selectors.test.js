999845398981ebec90ce569a23b1517d25a09ae7
Михаил Кононенко <vbifonixor@gmail.com>
2017-09-09 15:03:05 +0300

    Написаны тесты для селекторов ленты #WF-120

--- src/redux/feed/selectors.test.js
+++ src/redux/feed/selectors.test.js
@@ -1,12 +1,55 @@
-import { getFeedByFilters } from './selectors'
+import { getFeedByFilters, getGroupedFeedListByCategory } from './selectors'
 
+const list = [
+  {
+    categoryGroup: 'health',
+    category: 'asthma',
+    day: 0,
+  },
+  {
+    categoryGroup: 'health',
+    category: 'heart',
+    day: 1,
+  },
+  {
+    categoryGroup: 'motorists',
+    category: 'badVisibility',
+    day: 0,
+  },
+  {
+    categoryGroup: 'health',
+    category: 'asthma',
+    day: 1,
+  }]
 
 describe('getFeedByFilters selector', () => {
-  const list = [{ categoryGroup: 'health' }, { categoryGroup: 'health' }, { categoryGroup: 'motorists' }]
-  const filters = [{ name: 'health', active: true }, { name: 'motorists', active: false }]
   describe('when we have health filter enabled', () => {
+    const filters = [{ name: 'health', active: true }, { name: 'motorists', active: false }]
     it('should return only health cards', () => {
-      expect(getFeedByFilters(list, filters)).toHaveLength(2)
+      expect(getFeedByFilters(list, filters)).toHaveLength(3)
+    })
+  })
+  describe('when we have all filters enabled', () => {
+    const filters = [{ name: 'all', active: true }]
+    it('should return all cards', () => {
+      expect(getFeedByFilters(list, filters)).toHaveLength(4)
+    })
+  })
+})
+
+describe('getGroupedFeedListByCategory selector', () => {
+  describe('should return an array of arrays', () => {
+    it('should have one array with two asthma cards for different days', () => {
+      expect(getGroupedFeedListByCategory(list))
+        .toContainEqual([
+          expect.objectContaining({
+            category: 'asthma',
+            day: 0,
+          }),
+          expect.objectContaining({
+            category: 'asthma',
+            day: 1,
+          })])
     })
   })
 })
