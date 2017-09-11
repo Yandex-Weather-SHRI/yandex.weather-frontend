f3e19afb9387dab2fe32a1fa272d859704445fae
Eugeniy <yarastqt@gmail.com>
2017-09-11 21:04:16 +0300

    Добавили тип

--- src/constants/feedItemType.js
+++ src/constants/feedItemType.js
@@ -1,4 +1,5 @@
 export const feedItemType = {
   alert: 'alert', // карточки, пришедшие с бэка по настройкам пользователя.
   notice: 'notice', // синяя карточки "Хотите больше советов?", ведущая в настройки
+  suggestedAlert: 'suggestedAlert',
 }
