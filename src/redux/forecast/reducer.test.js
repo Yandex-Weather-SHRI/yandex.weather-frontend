32eae4babd0c25d22f39a65e3b51fdf61d0fc77d
Михаил Кононенко <vbifonixor@gmail.com>
2017-09-11 20:37:18 +0300

    Написал тест для редьюсера прогноза #WF-120

---
+++ src/redux/forecast/reducer.test.js
@@ -0,0 +1,33 @@
+import { forecastReducer } from './reducer'
+import { weatherGetSuccess } from './actions'
+
+describe('forecastReducer', () => {
+  const payload = {
+    fact: {
+      condition: 'пасмурно',
+      feels_like: 19,
+      humidity: 64,
+      icon: 'bkn_d',
+      pressure_mm: 748,
+      soil_temp: 17,
+      temp: 19,
+      wind_speed: 1,
+    },
+    geo_object: {
+      locality: { id: 213, name: 'Москва' },
+    },
+  }
+
+  const initialState = {
+    fetching: true,
+    fetched: false,
+  }
+
+  it('should put weather to state', () => {
+    expect(forecastReducer(initialState, weatherGetSuccess(payload))).toEqual(expect.objectContaining({
+      fetching: false,
+      fetched: true,
+      ...payload,
+    }))
+  })
+})
