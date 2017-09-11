6eaff3ab24447a158bcae7d48d96d3e869a69542
Eugeniy <yarastqt@gmail.com>
2017-09-11 14:53:22 +0300

    Сделал у компонента Loader фикс. позиционирование

--- src/ui/organisms/PageLoader/PageLoader.js
+++ src/ui/organisms/PageLoader/PageLoader.js
@@ -18,7 +18,13 @@ const Container = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-  flex: 1 0 auto;
+  background-color: rgba(255, 255, 255, 0.5);
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  z-index: 1000;
 `
 
 const LoaderIcon = styled(Icon)`
