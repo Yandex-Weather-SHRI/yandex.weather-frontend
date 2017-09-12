ae99a0b0d58136664fa879d613b395ff421a4525
Artem Maksimov <temamaksimov@gmail.com>
2017-09-12 18:17:54 +0300

    Исправил метод renderFeedItem #WF-145

--- src/ui/pages/FeedPage/FeedPage.js
+++ src/ui/pages/FeedPage/FeedPage.js
@@ -64,7 +64,7 @@ class FeedPageContainer extends Component {
     this.props.setFeedFilter({ name, active })
   }
 
-  renderFeedItem(item) {
+  renderFeedItem = (item) => {
     const type = Array.isArray(item) ? item[0].type : item.type
 
     switch (type) {
