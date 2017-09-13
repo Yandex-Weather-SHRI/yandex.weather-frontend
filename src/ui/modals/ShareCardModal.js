6f61d759a86772e46a063d9ea7835bac55c47ec8
Artem Maksimov <temamaksimov@gmail.com>
2017-09-13 16:29:12 +0300

    Исправил шеринг в соц. сети #WF-143

--- src/ui/modals/ShareCardModal.js
+++ src/ui/modals/ShareCardModal.js
@@ -10,8 +10,10 @@ import { SimpleModal as BaseSimpleModal } from './base/SimpleModal'
 
 const {
   TwitterShareButton,
-  TelegramShareButton,
   VKShareButton,
+  FacebookShareButton,
+  TelegramShareButton,
+  OKShareButton,
 } = ShareButtons
 
 const SOCIAL_BUTTONS = [
@@ -19,56 +21,109 @@ const SOCIAL_BUTTONS = [
     id: 'twitter',
     component: TwitterShareButton,
     icon: generateShareIcon('twitter'),
+    title: 'Твиттер',
   },
   {
     id: 'vk',
     component: VKShareButton,
     icon: generateShareIcon('vk'),
+    title: 'Вконтакте',
+  },
+  {
+    id: 'facebook',
+    component: FacebookShareButton,
+    icon: generateShareIcon('facebook'),
+    title: 'Facebook',
   },
   {
     id: 'telegram',
     component: TelegramShareButton,
     icon: generateShareIcon('telegram'),
+    title: 'Telegram',
+  },
+  {
+    id: 'ok',
+    component: OKShareButton,
+    icon: generateShareIcon('ok'),
+    title: 'Одноклассники',
   },
 ]
 
 const COMMON_ICON_PROPS = {
-  size: 64,
+  size: 24,
   round: true,
 }
 
-const SHARE_URL = 'https://yandex-weather.herokuapp.com/feed'
+const SHARE_URL = 'https://yandex-weather.herokuapp.com/'
 
 const SimpleModal = styled(BaseSimpleModal)`
-  padding: 24px;
   width: 280px;
   display: flex;
+  padding-top: 10px;
+  padding-bottom: 2px;
 `
 
-const getShareTitle = text =>
-  `${text.slice(0, 50)}...\
- больше полезных советов в категории на Яндекс.Погоде`
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+`
+
+const Text = styled.div`
+  margin-left: 16px;
+  font-size: 1.6rem;
+  letter-spacing: 0.2px;
+  text-align: left;
+  user-select: none;
+  color: rgba(0, 0, 0, 0.87);
+`
+
+const ButtonContent = styled.div`
+  display: flex;
+  padding: 12px 0;
+`
+
+const ButtonWrapper = styled.div`
+  .SocialMediaShareButton {
+    &:focus {
+      outline: none;
+    }
+  }
+`
 
 export const ShareCardModal = (props) => {
   const { card } = props.meta
 
   const buttons = SOCIAL_BUTTONS.map(button =>
-    <button.component
-      key={button.id}
-      url={SHARE_URL}
-      title={getShareTitle(card.text)}
-      description={card.text}
-    >
-      <button.icon {...COMMON_ICON_PROPS} />
-    </button.component>
+    <ButtonWrapper>
+      <button.component
+        key={button.id}
+        url={SHARE_URL}
+        title={card.text}
+        description={card.text}
+        // eslint-disable-next-line max-len
+        image="https://raw.githubusercontent.com/Yandex-Weather-SHRI/yandex.weather-frontend/master/src/assets/images/share-images/img-share%403x.png"
+      >
+        <ButtonContent>
+          <button.icon {...COMMON_ICON_PROPS} />
+          <Text>{button.title}</Text>
+        </ButtonContent>
+      </button.component>
+    </ButtonWrapper>
+  )
+
+  const modalMessageContent = (
+    <Container>
+      {buttons}
+    </Container>
   )
 
   return (
     <SimpleModal>
       <ModalMessage
         title="Поделиться"
-        content={buttons}
-        contentStyle="justify-content: center;"
+        content={modalMessageContent}
+        contentStyle=""
       />
     </SimpleModal>
   )
