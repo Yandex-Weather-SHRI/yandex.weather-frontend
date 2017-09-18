919038656e56ee7d45b87f99ccbfa451719557a1
Eugeniy <yarastqt@gmail.com>
2017-09-18 17:43:19 +0300

    Удалил не нужные компоненты

--- src/ui/organisms/FeedCard/metas/index.js
+++
@@ -1,4 +0,0 @@
-export { FeedCardMetaHeart } from './FeedCardMetaHeart'
-export { FeedCardMetaAsthma } from './FeedCardMetaAsthma'
-export { FeedCardMetaJoint } from './FeedCardMetaJoint'
-export { FeedCardMetaHead } from './FeedCardMetaHead'
